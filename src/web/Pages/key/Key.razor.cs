536ed5d2a0a2754c79d18e40ab78b60d847feb0a
Gabriel Santos <gabriel.santos@targetsoftware.com.br>
2023-06-08 10:07:42 -0300

    adjusts in .vscode files

--- src/web/Pages/key/Key.razor.cs
+++ src/web/Pages/key/Key.razor.cs
@@ -4,7 +4,7 @@ public partial class Key : ComponentBase
     public string KeyFileName { get; set; } = Guid.NewGuid().ToString();
     public string KeyPassword { get; set; } = "";
     public int KeySize { get; set; } = 2048;
-    public string KeyAsBlob { get; set; } = "";
+    public string KeyContents { get; set; } = "";
 
     [Inject]
     private KeyService _keyService { get; set; }
@@ -12,6 +12,6 @@ public partial class Key : ComponentBase
     public void GenerateNewKey() 
     {
         _keyService.NewKey(KeyPassword, KeySize);
-        KeyAsBlob = _keyService.GetKey(EKeyType.BlobString, KeyPassword);
+        KeyContents = _keyService.GetKey(EKeyType.PEM, KeyPassword);
     }
 }
